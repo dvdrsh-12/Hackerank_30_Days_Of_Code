27d6632b84f61a850970fda94d4a67c03ec41e89
Devadarsh M R <devadarshmr12567@gmail.com>
2024-02-23 21:27:16 +0530

    Day:0 Solution Added

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+
+// Day:0 - Hello, World.
+
+import java.util.*;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(str);
+
+        sc.close();
+
+    }
+}
