23e68b7a51df23726d0100a8627251720cb8ca09
Devadarsh M R <devadarshmr12567@gmail.com>
2024-02-24 13:03:24 +0530

    Day:3 Solution Added

---
+++ Solution4.java
@@ -0,0 +1,28 @@
+
+// Day:3 - Intro to Conditional Statements.
+
+import java.util.*;
+
+public class Solution4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n >= 1 && n <= 100) {
+            if (n % 2 != 0) {
+                System.out.println("Weird");
+            } else {
+                if (n >= 2 && n <= 5) {
+                    System.out.println("Not Weird");
+                } else if (n >= 6 && n <= 20) {
+                    System.out.println("Weird");
+                } else if (n > 20) {
+                    System.out.println("Not Weird");
+                }
+            }
+        }
+        sc.close();
+    }
+}
